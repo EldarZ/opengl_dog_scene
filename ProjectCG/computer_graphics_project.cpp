70b77bb798bcfd4e4d21795da72d4bf56dc1b30f
eldarz <kaleldar@gmail.com>
2018-07-28 17:52:15 +0300

    added lighting

--- ProjectCG/computer_graphics_project.cpp
+++ ProjectCG/computer_graphics_project.cpp
@@ -4,13 +4,20 @@
 using namespace std;
 
 
+GLfloat Eldar = 3.0f;
+
+GLfloat slate_ambient[] = { 0.02f, 0.02f, 0.02f }, 
+slate_diffuse[] = { 0.02f, 0.01f, 0.01f }, 
+slate_specular[] = { 0.4f, 0.4f, 0.4f }, 
+slate_shininess = .78125f;
+
 class Camera
 {
 public:
-	Camera() :x(0), y(5), z(10) {};
+	Camera() :x(0), y(5), z(10), r(10), alpha(0) {};
 	double x,y,z;
-	const double r = 10;
-	double alpha = 0;
+	double r;
+	double alpha;
 };
 
 Camera gCamera;
@@ -18,35 +25,27 @@ Camera gCamera;
 class Floor
 {
 public:
-	double startX, startZ, w, h;
-	GLfloat color1[3]{ 0.1f,0.1f,0.1f };
-	GLfloat color2[3]{ 0.7f,0.7f,0.7f };
-	Floor() : startX(-5), startZ(-5), w(5), h(5) {};
+	int startX, startZ, w, h;
+	GLfloat color1[3];
+	GLfloat color2[3];
+	Floor() : startX(-5), startZ(-5), w(5), h(5) 
+	{
+		color1[0] = 0.1f; color1[1] = 0.1f;	color1[2] = 0.1f;
+		color2[0] = 0.7f; color2[1] = 0.7f;	color2[2] = 0.7f;
+	};
 
 	void draw()
 	{
+		glPushMatrix();
 		glBegin(GL_QUADS);
+		glMaterialfv(GL_FRONT, GL_AMBIENT, slate_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, slate_diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, slate_specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, slate_shininess);
 
 		for (int x = startX; x < w; x++) {
 			for (int z = startZ; z < h; z++) {
-				if (z % 2 == 0)
-				{
-					if (x % 2 == 0) {
-						glColor3fv(color1);
-					}
-					else {
-						glColor3fv(color2);
-					}
-				}
-				else {
-					if (x % 2 == 0) {
-						glColor3fv(color2);
-					}
-					else {
-						glColor3fv(color1);
-					}
-				}
-				//glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (x + z) % 2 == 0 ? RED : WHITE);
+				glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, (x + z) % 2 == 0 ? color1 : color2);
 				glVertex3d(x, 0, z);
 				glVertex3d(x + 1, 0, z);
 				glVertex3d(x + 1, 0, z + 1);
@@ -55,6 +54,7 @@ public:
 		}
 
 		glEnd();
+		glPopMatrix();
 	}
 };
 
@@ -65,20 +65,61 @@ class Dog
 public:
 	void draw() {
 		glPushMatrix();
-		GLfloat color[3]{ 1.0f,0.0f,0.0f };
+		GLfloat color[3] = { 1.0f,0.0f,0.0f };
 		glColor3fv(color);
-		//glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
-		glTranslated(1, 1, 1);
-		glutSolidSphere(1, 30, 30);
+		
+		glMaterialfv(GL_FRONT, GL_AMBIENT, slate_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, slate_diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, slate_specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, slate_shininess);
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
 
 		glPushMatrix();
+		glTranslated(0, 1, 0);
+		glScalef(1, 1, 2);
+		glutSolidCube(1);
+		glPopMatrix();
+
+
+		glPopMatrix();
 	}
 };
 
 Dog gDog;
 
-// Moves the camera according to the key pressed, then ask to refresh the
-// display.
+class Lamp
+{
+public:
+	GLfloat ambient[3] = { 0.1f, 0.1f, 0.1f };
+	GLfloat color[4] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat position[4] = { 0.0, 3.0,3.0 , 0.0};
+
+	void init() 
+	{
+		glEnable(GL_LIGHT0);
+	}
+
+	void draw() {
+		position[1] = Eldar;
+
+		glPushMatrix();
+		glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+		glLightfv(GL_LIGHT0, GL_SPECULAR, color);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, color);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+		
+		glColor3fv(color);
+		GLfloat ambient[] = {10.0, 10.0, 10.0, 1.0 };
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+		glTranslatef(position[0], position[1], position[2]);
+		glutSolidSphere(0.2, 100, 100);
+
+		glPopMatrix();
+	}
+};
+Lamp gLamp;
+
 void special(int key, int, int) {
 	switch (key) {
 	case GLUT_KEY_LEFT: 
@@ -93,6 +134,15 @@ void special(int key, int, int) {
 		break;
 	case GLUT_KEY_UP: gCamera.y += 1; break;
 	case GLUT_KEY_DOWN: gCamera.y -= 1; break;
+	case GLUT_KEY_F1:
+		Eldar += 0.3;
+		break;
+	case GLUT_KEY_F2:
+		Eldar -= 0.3;
+		break;
+	case GLUT_KEY_F3:
+		exit(0);
+		break;
 	}
 	glutPostRedisplay();
 }
@@ -119,10 +169,12 @@ void display() {
 	}*/
 
 	gluLookAt(gCamera.x, gCamera.y, gCamera.z, 0, 0, 0, 0, 1, 0);
-
-	gFloor.draw();
+	
+	gLamp.draw();
+	
 	gDog.draw();
-
+	gFloor.draw();
+	
 
 	glFlush();
 	glutSwapBuffers();
@@ -137,6 +189,10 @@ int main(int argc, char** argv) {
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutSpecialFunc(special);
+	glEnable(GL_LIGHTING);
+	gLamp.init();
+	glEnable(GL_DEPTH_TEST);
+
 	glutMainLoop();
 	return 0;
 }
