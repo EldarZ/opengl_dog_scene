8ff391a387d19ab8131c9706191596c7f8e85c7e
Eldar <kaleldar@gmail.com>
2018-07-28 20:55:31 +0300

    added dog model

--- ProjectCG/computer_graphics_project.cpp
+++ ProjectCG/computer_graphics_project.cpp
@@ -3,7 +3,6 @@
 #include <GL\freeglut.h>
 using namespace std;
 
-
 GLfloat Eldar = 3.0f;
 
 GLfloat slate_ambient[] = { 0.02f, 0.02f, 0.02f }, 
@@ -65,21 +64,114 @@ class Dog
 public:
 	void draw() {
 		glPushMatrix();
-		GLfloat color[3] = { 1.0f,0.0f,0.0f };
+		GLfloat color[3] = { 0.92, 0.814, 0.382 };
 		glColor3fv(color);
-		
-		glMaterialfv(GL_FRONT, GL_AMBIENT, slate_ambient);
-		glMaterialfv(GL_FRONT, GL_DIFFUSE, slate_diffuse);
-		glMaterialfv(GL_FRONT, GL_SPECULAR, slate_specular);
-		glMaterialf(GL_FRONT, GL_SHININESS, slate_shininess);
+
+		GLfloat dog_ambient[] = { 0.001f, 0.001f, 0.001f },
+			dog_diffuse[] = { 0.0001f, 0.0001f, 0.0001f },
+			dog_specular[] = { 0.1f, 0.1f, 0.1f },
+			dog_shininess = -1.0f;
+
+		glMaterialfv(GL_FRONT, GL_AMBIENT, dog_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, dog_diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, dog_specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, dog_shininess);
+		glMaterialf(GL_FRONT, GL_EMISSION, 0);
 		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
 
+		//scale to model with higher values
+		glScalef(0.3, 0.3, 0.3);
+		glTranslated(0, 3.5, 0);
+		
 		glPushMatrix();
-		glTranslated(0, 1, 0);
-		glScalef(1, 1, 2);
+		glScalef(3, 3, 5);
 		glutSolidCube(1);
 		glPopMatrix();
 
+		//torso
+		glPushMatrix();
+		glTranslated(-1, -2.5, -2);
+		glScalef(1, 2, 1);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		//legs
+		glPushMatrix();
+		glTranslated(1, -2.5, -2);
+		glScalef(1, 2, 1);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslated(1, -2.5, 2);
+		glScalef(1, 2, 1);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslated(-1, -2.5, 2);
+		glScalef(1, 2, 1);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		//tail
+		glPushMatrix();
+		glTranslated(0, 0.5, -3.3);
+		glRotatef(-30, 1, 0, 0);
+		glScalef(0.5, 0.5, 1.8);
+		
+		glutSolidCube(1);
+		glPopMatrix();
+
+		glPushMatrix();
+		glRotatef(10, 1, 0, 0);
+
+		//head
+		glPushMatrix();
+		glTranslated(0, 2.5, 3);
+		glScalef(2, 2.2, 3);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		//ears
+		glPushMatrix();
+		glTranslated(-0.8, 3.7, 2.6);
+		glScalef(0.5, 1, 0.5);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslated(0.8, 3.7, 2.6);
+		glScalef(0.5, 1, 0.5);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		//eyes
+		GLfloat eyes_ambient[] = { 0.02f, 0.02f, 0.02f },
+			eyes_diffuse[] = { 0.02f, 0.01f, 0.01f },
+			eyes_specular[] = { 0.4f, 0.4f, 0.4f },
+			eyes_shininess = .0;
+		GLfloat black[] = { 0,0,0 };
+		glColor3fv(black);
+		glMaterialfv(GL_FRONT, GL_AMBIENT, eyes_ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, eyes_diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, eyes_specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, eyes_shininess);
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, black);
+
+		glPushMatrix();
+		glTranslated(0.5, 2.6, 4.5);
+		glScalef(0.25, 0.25, 0.25);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslated(-0.5, 2.6, 4.5);
+		glScalef(0.25, 0.25, 0.25);
+		glutSolidCube(1);
+		glPopMatrix();
+
+		glPopMatrix();
 
 		glPopMatrix();
 	}
@@ -92,7 +184,7 @@ class Lamp
 public:
 	GLfloat ambient[3] = { 0.1f, 0.1f, 0.1f };
 	GLfloat color[4] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat position[4] = { 0.0, 3.0,3.0 , 0.0};
+	GLfloat position[4] = { -3.0, 3.0,3.0 , 0.0};
 
 	void init() 
 	{
@@ -160,21 +252,11 @@ void display() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	/*gluLookAt(camera.getX(), camera.getY(), camera.getZ(),
-		checkerboard.centerx(), 0.0, checkerboard.centerz(),
-		0.0, 1.0, 0.0);*/
-	/*checkerboard.draw();
-	for (int i = 0; i < sizeof balls / sizeof(Ball); i++) {
-		balls[i].update();
-	}*/
-
 	gluLookAt(gCamera.x, gCamera.y, gCamera.z, 0, 0, 0, 0, 1, 0);
 	
 	gLamp.draw();
-	
 	gDog.draw();
 	gFloor.draw();
-	
 
 	glFlush();
 	glutSwapBuffers();
